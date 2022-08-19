474b32e38e2280aa4aa77b1c547e458b7a195548
huytran <thtran.082@gmail.com>
2022-08-19 14:14:29 +0700

    chore: remove redundant declaration

--- projects/demo/src/modules/app/getting-started/getting-started.module.ts
+++ projects/demo/src/modules/app/getting-started/getting-started.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { VecnaTableModule } from "@vecna/core";
 import { GettingStartedComponent } from './getting-started.component';
 import { GettingStartedRoutingModule } from './getting-started.routes';
 
 @NgModule({
   declarations: [GettingStartedComponent],
   exports: [GettingStartedComponent],
-  imports: [GettingStartedRoutingModule, VecnaTableModule],
+  imports: [GettingStartedRoutingModule],
 })
 export class GettingStartedModule {}
