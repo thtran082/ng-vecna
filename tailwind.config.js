69f59349e7d9b2363c817264bd5e137e799ea446
huytran <thtran.082@gmail.com>
2022-08-30 00:07:29 +0700

    style(tailwind): setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,23 @@ module.exports = {
     extend: {},
     fontFamily: {
       ibm: ["IBM Plex Mono"],
+    },
+    fontSize: {
+      h1: ['3.875rem', { lineHeight: '4.875rem', letterSpacing: '0' }],
+      h2: ['3rem', { lineHeight: '3.75rem', letterSpacing: '0' }],
+      h3: ['2rem', { lineHeight: '2.5rem', letterSpacing: '0' }],
+      h4: ['1.75rem', { lineHeight: '2.25rem', letterSpacing: '0' }],
+      h5: ['1.5rem', { lineHeight: '1.875rem', letterSpacing: '0' }],
+      h6: ['1.25rem', { lineHeight: '1.625rem', letterSpacing: '0' }],
+      h7: ['1rem', { lineHeight: '1.5rem', letterSpacing: '0' }],
+      h8: ['0.875rem', { lineHeight: '1.25rem', letterSpacing: '0' }],
+      h9: ['0.75rem', { lineHeight: '1rem', letterSpacing: '0.01rem' }],
+      h10: ['0.625rem', { lineHeight: '1rem', letterSpacing: '0.02rem' }],
+      b1: ['1.125rem', { lineHeight: '1.5rem', letterSpacing: '0' }],
+      b2: ['1rem', { lineHeight: '1.5rem', letterSpacing: '0' }],
+      b3: ['0.875rem', { lineHeight: '1.25rem', letterSpacing: '0' }],
+      b4: ['0.75rem', { lineHeight: '1rem', letterSpacing: '0.01rem' }],
+      b5: ['0.625rem', { lineHeight: '1rem', letterSpacing: '0.02rem' }],
     }
   },
   plugins: [],
