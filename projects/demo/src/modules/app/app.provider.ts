b650776883a0838895c76c133fcd473a3546c801
huytran <thtran.082@gmail.com>
2022-09-11 11:50:08 +0700

    feat(demo): add logo

--- projects/demo/src/modules/app/app.provider.ts
+++ projects/demo/src/modules/app/app.provider.ts
@@ -1,10 +1,15 @@
-import { Provider } from "@angular/core";
-import { VECNA_DOC_PAGES } from "@vecna/addon-doc/tokens";
-import { pages } from "./pages";
+import { Provider } from '@angular/core';
+import { VECNA_DOC_LOGO, VECNA_DOC_PAGES } from '@vecna/addon-doc/tokens';
+import { LogoComponent } from './logo/logo.component';
+import { pages } from './pages';
 
 export const APP_PROVIDERS: Provider[] = [
- {
-  provide: VECNA_DOC_PAGES,
-  useValue: pages,
- }
-]
+  {
+    provide: VECNA_DOC_PAGES,
+    useValue: pages,
+  },
+  {
+    provide: VECNA_DOC_LOGO,
+    useClass: LogoComponent,
+  },
+];
---
+++ projects/demo/src/modules/app/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: `app-logo`,
+  templateUrl: `./logo.component.html`,
+  styleUrls: [`./logo.component.css`],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LogoComponent {
+  constructor() {}
+}
